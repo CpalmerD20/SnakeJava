832e6b472a534cc9c6238fb779040d26799d754e
CpalmerD20 <cheynepalmer@gmail.com>
2022-01-08 14:52:03 -0800

    added enum for direction

--- src/Snake.java
+++ src/Snake.java
@@ -4,29 +4,30 @@ import java.awt.event.KeyListener;
 
 public class Snake implements KeyListener {
 
-  final static int X[] = new int [Map.MAP_UNITS];
-  final static int Y[] = new int [Map.MAP_UNITS];
-  static String direction = "East";
+  final static int X[] = new int [Map.GRID_MAX];
+  final static int Y[] = new int [Map.GRID_MAX];
+  enum Direction {North, South, East, West};
+  static Direction d;
   static Color snakeHead = Color.GREEN;
-  static Color snakeBody = new Color(45,180,0);
-  static int bodyLength = 5;
+  static Color snakeBody = new Color(50,200,0);
+  static int bodyLength = 4;
   static int foodEaten = 0;
   
-  public static void collide() {
-    //body
+  static void collide() {
+    //collision with body
     for(int i = bodyLength; i > 0; i--) {
       if((X[0] == X[i] && Y[0] == Y[i])) {Game.running = false;}
-    //borders
+    //collision with borders
       if(X[0] < 0) {Game.running = false;}
       if(Y[0] < 0) {Game.running = false;}
       if(X[0] > Map.WIDTH) {Game.running = false;}
       if(Y[0] > Map.HEIGHT) {Game.running = false;}
     }
-    Game.turn++;
+    Game.over = true;
   }
   
-  public static void eatFood() {
-    if((X[0] == Food.xLoc && Y[0] == Food.yLoc)) {
+  static void eatFood() {
+    if((X[0] == Food.xLocation && Y[0] == Food.yLocation)) {
       bodyLength++;
       foodEaten++;
       Food.placeFood();
@@ -34,43 +35,43 @@ public class Snake implements KeyListener {
     }
   }
   
-  public static void move() {
+  static void move() {
     for(int i = bodyLength; i > 0; i--) {
       X[i] = X[i-1];
       Y[i] = Y[i-1];
     }
-    switch(direction) {
-      case "North" -> Y[0] = Y[0] - Map.GRID_UNIT;
-      case "South" -> Y[0] = Y[0] + Map.GRID_UNIT;
-      case "West" -> X[0] = X[0] - Map.GRID_UNIT;
-      case "East" -> X[0] = X[0] + Map.GRID_UNIT;
+    switch(d) {
+      case North -> Y[0] = Y[0] - Map.GRID_UNIT;
+      case South -> Y[0] = Y[0] + Map.GRID_UNIT;
+      case West -> X[0] = X[0] - Map.GRID_UNIT;
+      case East -> X[0] = X[0] + Map.GRID_UNIT;
     }
   }
   
   @Override
   public void keyPressed(KeyEvent e) {
     switch(e.getKeyCode()) {
-      case KeyEvent.VK_LEFT: if(direction != "East") {direction = "West";} break;
-      case KeyEvent.VK_RIGHT: if(direction != "West") {direction = "East";} break;
-      case KeyEvent.VK_UP: if(direction != "South") {direction = "North";} break;
-      case KeyEvent.VK_DOWN: if(direction != "North") {direction = "South";} break;
+      case KeyEvent.VK_UP: if(d != Direction.South) {d = Direction.North;} break;
+      case KeyEvent.VK_DOWN: if(d != Direction.North) {d = Direction.South;} break;
+      case KeyEvent.VK_LEFT: if(d != Direction.East) {d = Direction.West;} break;
+      case KeyEvent.VK_RIGHT: if(d != Direction.West) {d = Direction.East;} break;
     }
-  }
-  @Override
-  public void keyTyped(KeyEvent e) {
-  }
-  @Override
-  public void keyReleased(KeyEvent e) {
   }
 
-  public static void reset() {
-    direction = "East";
+  static void create() {
+    d = Direction.East;
     foodEaten = 0;
-    bodyLength = 5;
+    bodyLength = 4;
     Game.score = "Score " + foodEaten;
     for (int i = 0; i < bodyLength; i++) {
-      X[i] = -i;    
-      Y[i] = 0;    
+      X[i] = -i;
+      Y[i] = 12 * Map.GRID_UNIT; //halfway down the map
     }
   }
+  
+  @Override
+  public void keyTyped(KeyEvent e) {}
+  
+  @Override
+  public void keyReleased(KeyEvent e) {}
 }
